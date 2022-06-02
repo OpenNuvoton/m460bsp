be1d1ecdf54524884cf7804afe6a70b3c095cfd5
WSChang0 <wschang0@nuvoton.com>
2022-06-02 14:57:59 +0800

    Fix build error of XOM IAR projects
    Change-Id: I634dc7e770166662a99ffb7c0f98689399f0f8bd

--- SampleCode/XOM/XOMLib/xomlib.c
+++ SampleCode/XOM/XOMLib/xomlib.c
@@ -16,10 +16,10 @@
 
 
 int32_t (*XOM_Add)(int32_t a, int32_t b) = (int32_t (*)(int32_t a, int32_t b))(0x00010001);
-int32_t (*XOM_Div)(int32_t a, int32_t b) = (int32_t (*)(int32_t a, int32_t b))(0x0001001d);
-int32_t (*XOM_Mul)(int32_t a, int32_t b) = (int32_t (*)(int32_t a, int32_t b))(0x0001003d);
-int32_t (*XOM_Sub)(int32_t a, int32_t b) = (int32_t (*)(int32_t a, int32_t b))(0x00010059);
-int32_t (*XOM_Sum)(int32_t *pbuf, int32_t n) = (int32_t (*)(int32_t *pbuf, int32_t n))(0x00010075);
+int32_t (*XOM_Div)(int32_t a, int32_t b) = (int32_t (*)(int32_t a, int32_t b))(0x00010015);
+int32_t (*XOM_Mul)(int32_t a, int32_t b) = (int32_t (*)(int32_t a, int32_t b))(0x0001002d);
+int32_t (*XOM_Sub)(int32_t a, int32_t b) = (int32_t (*)(int32_t a, int32_t b))(0x00010041);
+int32_t (*XOM_Sum)(int32_t *pbuf, int32_t n) = (int32_t (*)(int32_t *pbuf, int32_t n))(0x00010055);
 
 #if defined(__ARMCC_VERSION)
 #pragma clang diagnostic pop
