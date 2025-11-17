3351191dffa211dcd128e11fd7a4167e71b63a05
Wayne <wclin@nuvoton.com>
2025-11-17 11:35:25 +0800

    Suppress warnings when building the project on Linux.
    1. Resolve symbol redefinition issues.
    2. Correct upper/lowercase filename and path mismatches.
    3. Remove or disable the unused toolchain.id configuration item.
    
    Change-Id: Icd34c48afb37161a10da58d3308af3176cd3c970

--- ThirdParty/mbedtls-3.1.0/library/common.h
+++ ThirdParty/mbedtls-3.1.0/library/common.h
@@ -66,7 +66,9 @@ extern void (*mbedtls_test_hook_test_fail)( const char * test, int line, const c
  * Although structs defined in header files are publicly available,
  * their members are private and should not be accessed by the user.
  */
+#ifndef MBEDTLS_ALLOW_PRIVATE_ACCESS
 #define MBEDTLS_ALLOW_PRIVATE_ACCESS
+#endif
 
 /** Byte Reading Macros
  *
